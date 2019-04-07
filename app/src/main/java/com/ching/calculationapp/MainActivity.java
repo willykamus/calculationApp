b311b9e21a8083ff59c232fbb269edd95f0aec28
William Ching <willykamus@gmail.com>
2019-04-06 23:50:29 -0400

    UI Design proposal

--- app/src/main/java/com/ching/calculationapp/MainActivity.java
+++ app/src/main/java/com/ching/calculationapp/MainActivity.java
@@ -3,11 +3,18 @@ package com.ching.calculationapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import model.Question;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
+
+
+
 }
--- app/src/main/java/model/Question.java
+++ app/src/main/java/model/Question.java
@@ -1,7 +1,39 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Question {
 
-    private int 
+    private static List<Question> questionList = new ArrayList<>();
 
+    private int result;
+    private String text;
+
+    public Question(int result, String text) {
+        this.result = result;
+        this.text = text;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public static Question getQuestion(int index){
+
+        return questionList.get(index);
+
+    }
 }
